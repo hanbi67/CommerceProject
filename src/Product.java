cba24ee23a9b1346303a351754b409acb7294490
Lee Hanbi <hanbi6717@naver.com>
2025-12-18 07:33:56 +0900

    feat: step1 완료

---
+++ src/Product.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Product {
+    //설명 : 개별 상품을 관리하는 클래스입니다. 현재는 전자제품만 관리합니다.
+    //상품명, 가격, 설명, 재고수량 필드를 갖습니다.
+
+    private String name;
+    private Integer price;
+    private String description;
+    private Integer stock;
+
+    //생성자
+    public Product(String name, Integer price, String description, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.stock = stock;
+    }
+
+    //출력용 메서드
+    public void printInfo() {
+        System.out.printf("%-15s | %,10d원 | %s%n", name, price, description);
+    }
+
+}
